4579fa5523d76d4f7d2a86876cadc6cee1aca74d
yujiosaka <yuji.i@doboken.net>
2016-01-18 01:39:29 +0900

    support getMin, getMax, getArgMin, getArgMax

--- src/calculations.precision.js
+++ src/calculations.precision.js
@@ -24,3 +24,75 @@ Matrix.prototype.getSum = function(value) {
   
   return adder(sum); 
 };
+
+Matrix.prototype.getMax = function() {
+ var thisData = this.data,
+   rows = this.rows,
+   cols = this.cols,
+   max = thisData[0][0];
+
+ for (var i = 0; i<rows; ++i) {
+   for (var j = 0; j<cols; ++j) {
+     if (thisData[i][j] > max) {
+       max = thisData[i][j];
+     }
+   }
+ }
+
+ return max;
+};
+
+Matrix.prototype.getMin = function() {
+ var thisData = this.data,
+   rows = this.rows,
+   cols = this.cols,
+   min = thisData[0][0];
+
+ for (var i = 0; i<rows; ++i) {
+   for (var j = 0; j<cols; ++j) {
+     if (thisData[i][j] < min) {
+       min = thisData[i][j];
+     }
+   }
+ }
+
+ return min;
+};
+
+Matrix.prototype.getArgMax = function() {
+ var thisData = this.data,
+   rows = this.rows,
+   cols = this.cols,
+   max = thisData[0][0],
+   idx = 0;
+
+ for (var i = 0; i<rows; ++i) {
+   for (var j = 0; j<cols; ++j) {
+     if (thisData[i][j] > max) {
+       max = thisData[i][j];
+       idx = cols * i + j;
+     }
+   }
+ }
+
+ return idx;
+};
+
+Matrix.prototype.getArgMin = function() {
+ var thisData = this.data,
+   rows = this.rows,
+   cols = this.cols,
+   min = thisData[0][0],
+   idx = 0;
+
+ for (var i = 0; i<rows; ++i) {
+   for (var j = 0; j<cols; ++j) {
+     if (thisData[i][j] < min) {
+       min = thisData[i][j];
+       idx = cols * i + j;
+     }
+   }
+ }
+
+ return idx;
+};
