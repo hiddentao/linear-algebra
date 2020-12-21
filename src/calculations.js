bd68488855aa96f799a6c38082b69c115b7f1bb1
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-12-22 01:01:45 +0330

    implement elimination

--- src/calculations.js
+++ src/calculations.js
@@ -18,26 +18,45 @@ Matrix.prototype.getInverse = function() {
     var matrix = this.data,
     rows = this.rows,
     cols = this.cols;
+    let temp;
     if(cols != rows){
       _throwError('matrix should be squared');
     }
 
-    for (i = 0; i < rows; i++) {
- 
-      for (j = 0; j < 2*rows; j++) {
+    order = rows;
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < 2*rows; j++) {
           if (j == (i + rows))
               matrix[i][j] = 1;
       }
+    }
+
+  for (let i = order - 1; i > 0; i--) {
+    if (matrix[i - 1][0] < matrix[i][0]) {
+        temp = matrix[i];
+        matrix[i] = matrix[i - 1];
+        matrix[i - 1] = temp;
+    }
   }
 
-  for (i = 0; i < rows; i++) {
- 
-    temp = matrix[i][i];
-    for (j = 0; j < 2 * rows; j++) {
+  for (let i = 0; i < order; i++) {
+     for (let j = 0; j < order; j++) {
+        if (j != i) {
+            temp = matrix[j][i] / matrix[i][i];
+            for (let k = 0; k < 2 * order; k++) {
+                matrix[j][k] -= matrix[i][k] * temp;
+            }
+        }
+      }
+  }
 
+  for (let i = 0; i < rows; i++) {
+    temp = matrix[i][i];
+    for (let j = 0; j < 2 * rows; j++) {
         matrix[i][j] = matrix[i][j] / temp;
     }
-}
+  }
 
  };
  
